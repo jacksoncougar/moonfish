07b4debf9e96767d892ce8d24e7a7dba5cb0fa1d
Jackson Cougar <jacksoncougar@gmail.com>
2013-02-02 07:13:14 -0700

    update

--- Moonfish/Moonfish.Core/Model/Vector3t.cs
+++ Moonfish/Moonfish.Core/Model/Vector3t.cs
@@ -15,35 +15,15 @@ namespace Moonfish.Core.Model
         public float X
         {
             get
-            {   //lets try twos compliment?
-                ushort radix = (ushort)((bits >> 00 & 0x7FF));
-                //if ((radix & 0x400) == 0x400) //negetive value
-                //{
-                    //what if there is not sign bit? would that make more sense?
-                    //x7ff would be max_value -
-                    //x400 would be... half value +//?????
-                    //3ff would be less than half
-                    //00 would be zero, then we offset by k, k? where k is half the range of values
-                    //0x7FF should result in ;must be negetive max?
-                    //0x400 should result in -1? 0?; positive max?
-                    // what should result in 1? 0x3FF??
-                    // what should result in 0? 0x000??
-                //if (radix == 0x7FF) return -1;
-                ////these values are negetive
-                //if (radix == 0x400) return 1;
-                ////these values are positve
-                //if (radix == 0) return 0;
-                if (radix == 0x7FF || radix == 0) return 0;//true? else return the value with a sign 
-                return (float)(radix - 0x3FF) / (float)0x3FF;
-                //}
-                //else
-                //{
-                //    short rr = (short)(radix);
-                //    return rr * xy_max_inverse;
-                //}
-                //if ((radix & 0x400) == 0x400) return -((~radix) & 0x000007FF) * xy_max_inverse;
-                ////if ((radix & 0x400) == 0x400) return (float)(~(radix & 0x3FF) + 1) * xy_max_inverse;
-                //else return (float)(radix & 0x3FF) * xy_max_inverse;
+            {
+                ushort radix = (ushort)(bits >> 00 & 0x7FF);                // retrieve the bits of this componant (first 11 bits)
+                if (radix == 0x7FF || radix == 0) return 0;                 // two special cases for zero: return zero on either
+                if ((radix & 0x400) == 0x400)                               // if sign bit is set, output should be negetive
+                    return -(float)(~(radix) & 0x3FF) * xy_max_inverse;     /* return the radix 'ones compliment' trimming the sign bit
+                                                                             * return the negetive value*/
+                else                                                        /* else just return the radix value multiplied by the pre-
+                                                                             * calculated ratio */
+                    return (float)(radix) * xy_max_inverse;
             }
             set
             {
@@ -56,9 +36,14 @@ namespace Moonfish.Core.Model
         {
             get
             {
-                short radix = (short)((bits >> 11 & 0x7FF));
-                if (radix == 0x7FF || radix == 0) return 0;//true? else return the value with a sign 
-                return (float)(radix - 0x3FF) / (float)0x3FF;
+                ushort radix = (ushort)(bits >> 00 & 0x7FF);                // retrieve the bits of this componant (first 11 bits)
+                if (radix == 0x7FF || radix == 0) return 0;                 // two special cases for zero: return zero on either
+                if ((radix & 0x400) == 0x400)                               // if sign bit is set, output should be negetive
+                    return -(float)(~(radix) & 0x3FF) * xy_max_inverse;     /* return the radix 'ones compliment' trimming the sign bit
+                                                                             * return the negetive value*/
+                else                                                        /* else just return the radix value multiplied by the pre-
+                                                                             * calculated ratio */
+                    return (float)(radix) * xy_max_inverse;
             }
             set
             {
@@ -71,9 +56,14 @@ namespace Moonfish.Core.Model
         {
             get
             {
-                short radix = (short)((bits >> 22 & 0x3FF));
-                if (radix == 0x3FF || radix == 0) return 0;//true? else return the value with a sign 
-                return (float)(radix - 0x1FF) / (float)0x1FF;
+                ushort radix = (ushort)(bits >> 00 & 0x3FF);                // retrieve the bits of this componant (first 10 bits)
+                if (radix == 0x7FF || radix == 0) return 0;                 // two special cases for zero: return zero on either
+                if ((radix & 0x200) == 0x200)                               // if sign bit is set, output should be negetive
+                    return -(float)(~(radix) & 0x1FF) * z_max_inverse;     /* return the radix 'ones compliment' trimming the sign bit
+                                                                             * return the negetive value*/
+                else                                                        /* else just return the radix value multiplied by the pre-
+                                                                             * calculated ratio */
+                    return (float)(radix) * z_max_inverse;
             }
             set
             {
